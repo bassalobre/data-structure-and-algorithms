52fdfbc94bf64ff3510b11eb213c65a7e99491b9
William Bassalobre <williambassalobre@gmail.com>
2024-04-11 16:53:32 -0300

    Add move zeroes challenge

--- leetcodes/js/moveZeroes.js
+++ leetcodes/js/moveZeroes.js
@@ -1,3 +1,16 @@
-const moveZeroes = (nums) => {
-    //
+const moveZeroes = (nums = []) => {
+    const zeroes = [];
+
+    let i = 0;
+    while (i < nums.length) {
+        if (nums[i] === 0) {
+            zeroes.push(...nums.splice(i, 1))
+        } else {
+            i++;
+        }
+    }
+
+    while (zeroes.length) {
+        nums.push(zeroes.pop());
+    }
 };
